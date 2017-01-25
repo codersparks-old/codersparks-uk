1a967b7bdbf3be9d35956e06df8e5de04d56bf2a
codersparks <codersparks@gmail.com>
2017-01-25 09:45:08 +0000

    Added SpringBootServletInitializer

--- codersparksukhome/src/main/java/com/github/codersparks/codersparksuk/Application.java
+++ codersparksukhome/src/main/java/com/github/codersparks/codersparksuk/Application.java
@@ -2,12 +2,19 @@ package com.github.codersparks.codersparksuk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by codersparks on 20/01/2017.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static final void main(String[] args) {
         SpringApplication.run(Application.class, args);
