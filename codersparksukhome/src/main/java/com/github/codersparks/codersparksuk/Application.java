01da8dbca800e03e53e3b76f02c673deb91539eb
codersparks <codersparks@gmail.com>
2017-01-27 10:50:44 +0000

    Added SpringSecurity Dialect to thymeleaf Added a "Dev" section on home page

--- codersparksukhome/src/main/java/com/github/codersparks/codersparksuk/Application.java
+++ codersparksukhome/src/main/java/com/github/codersparks/codersparksuk/Application.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by codersparks on 20/01/2017.
@@ -14,6 +22,22 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
+    }
+
+    @Bean
+    public TemplateEngine templateEngine(SpringSecurityDialect springSecurityDialect) {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+
+        Set<IDialect> dialectSet = new HashSet<>();
+        dialectSet.add(springSecurityDialect);
+        springTemplateEngine.setAdditionalDialects(dialectSet);
+
+        return springTemplateEngine;
+    }
+
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
     }
 
     public static final void main(String[] args) {
